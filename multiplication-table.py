3c64e5e49be8e829a099bc0d78c064830b7d0ab8
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-09-29 14:57:25 -0400

    Updated from Brython Server: 9/29/2015 2:57:25 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -24,10 +24,11 @@ Height of multiplication table: 8
 width = input("Width of multiplication table:")
 height = input("Height of the multiplication table:")
 a = len(str(int(width)*int(height)))
+print (a)
 
 for y in range(1, int(width)+1):
     for x in range(1, int(height)+1):
-        while len(str(a)) > len(str(y*x)): #while loop is not working
-            str(y*x + " ")
+        #while len(str(a)) > len(str(y*x)): #while loop is not working
+           # str(y*x + " ")
         print(y*x, end=' ')
         if x==len(range(0, int(width))): print()
